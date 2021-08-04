8a0600a6f511d700784be8db6bda5f500c890af0
pics78 <mcmc.k0206@gmail.com>
2021-08-04 20:33:36 +0900

    modify: Updating around deployment

--- compressingForLambda.sh
+++ compressingForLambda.sh
@@ -3,10 +3,10 @@
 cd $(dirname $0)
 
 mkdir temp
-cp -r $(ls -I README.md -I compressingForLambda.sh -I modules -I temp) modules/*  temp/
+cp -r $(ls -I README.md -I compressingForLambda.sh -I local -I modules -I temp) modules/*  temp/
 
 cd temp
+echo PRD > env/environment
 zip -r ../packaging.zip *
 
-cd ..
-rm -r temp/
+cd ..; rm -r temp/
