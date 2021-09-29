c6697f83676737e857b42c92a26ea08fcfbfadd5
pics78 <mcmc.k0206@gmail.com>
2021-09-29 21:29:54 +0900

    add ParseUtil class

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import datetime
 from connector import lineConnector, awsConnector
 from env.envMgr import getEnv
 from env.envKeyDef import Portal, StatusForRunning
+from utils.parseUtil import ParseUtil
 
 loginUrl        = getEnv(Portal.URL) + getEnv(Portal.LOGINPATH)
 userId          = getEnv(Portal.USR)
@@ -16,25 +17,22 @@ inputParamName2 = getEnv(Portal.PARAMNAME2)
 isTest = True if getEnv(StatusForRunning.MODE) == StatusForRunning.IS_TEST.value else False
 
 def lambda_handler(event, context):
-    ses = requests.session()
+    parseUtil = ParseUtil(requests.session(), getEnv(Portal.URL))
 
-    resForLogin = ses.request('GET', loginUrl)
-    soupForLogin = BeautifulSoup(resForLogin.text, 'html.parser')
-    inputParam1 = soupForLogin.find('input', attrs={'name': inputParamName1}).get('value')
-    inputParam2 = soupForLogin.find('input', attrs={'name': inputParamName2}).get('value')
-
+    loginSoup = parseUtil.getSoup('GET', getEnv(Portal.LOGINPATH))
+    param1 = loginSoup.findInSoup('input', attrs={'name': getEnv(Portal.PARAMNAME1)}).get('value')
+    param2 = loginSoup.findInSoup('input', attrs={'name': getEnv(Portal.PARAMNAME2)}).get('value')
     payload = {
         '__pjax': 'true',
         'UserID': userId,
         'Password': passwd,
         'BirthDay': '1111/11/11',
-        inputParamName1: inputParam1,
-        inputParamName2: inputParam2
+        inputParamName1: param1,
+        inputParamName2: param2
     }
 
-    resForHome = ses.request('POST', loginUrl, data=payload)
-    soupForHome = BeautifulSoup(resForHome.text, 'html.parser')
-    groupList = soupForHome.select('#innercontent > div[class="group"]')
+    groupList = parseUtil.getSoup('POST', getEnv(Portal.LOGINPATH), data=payload).selectInSoup(
+        '#innercontent > div[class="group"]')
     # 4番目のgroupクラスdiv要素がお知らせボックス
     infoList = groupList[3].select('.groupcontent > ul > li')
 
@@ -60,36 +58,30 @@ def lambda_handler(event, context):
         else:
             infoCount = 1
             for info in newInfo:
-                resForInfoDetail = ses.request('GET', getEnv(Portal.URL) + info.get('href'))
-                soupForInfoDetail = BeautifulSoup(resForInfoDetail.text, 'html.parser')
-                infoDetails = soupForInfoDetail.select('#innercontent > div[class="centerblock1"] > div[class="centerblock1content"] > table > tr')
+                infoDetails = parseUtil.getSoup('GET', info.get('href')).selectInSoup(
+                    '#innercontent > div[class="centerblock1"] > div[class="centerblock1content"] > table > tr')
                 infoContentList = []
                 fileList = []
                 fileCount = 1
                 for elem in infoDetails:
                     title = elem.select_one('td:nth-child(1)')
                     val = elem.select_one('td:nth-child(2)')
-                    if val.text.strip('\n') != '':
-                        fileName = val.text.strip('\n')
+                    valText = val.text.strip('\n')
+                    if valText != '':
                         infoContentList.append(f'<{title.text}>')
-                        infoContentList.append(fileName + '\n')
+                        infoContentList.append(valText + '\n')
                         valChildAnchor = val.select_one('a[class="jsDownload"]')
                         if valChildAnchor != None:
-                            hrefUrl = getEnv(Portal.URL) + valChildAnchor.get('href')
-                            urlData = ses.request('GET', hrefUrl).content
                             uploadFileName = f'{updateDate}_{infoCount}_{fileCount}.pdf'
-                            with open(uploadFileName ,mode='wb') as f:
-                                f.write(urlData)
-                            awsConnector.uploadFileToS3(uploadFileName)
-                            fileUri = awsConnector.getFileUrl(uploadFileName)
-                            fileList.append({
-                                'fileName': fileName,
-                                'uri': fileUri
-                            })
+                            if parseUtil.wget(valChildAnchor.get('href'), uploadFileName):
+                                awsConnector.uploadFileToS3(uploadFileName)
+                                fileList.append([
+                                    valText, awsConnector.getFileUrl(uploadFileName)
+                                ])
                             fileCount += 1
                 lineConnector.sendPortalNewInfo('\n'.join(infoContentList))
                 lineConnector.sendCarousel(
-                    list(map(lambda i: lineConnector.getCarouselColumn(i['fileName'], i['uri']), fileList)))
+                    list(map(lambda i: lineConnector.getCarouselColumn(i[0], i[1]), fileList)))
                 infoCount += 1
             awsConnector.updateLastNewsTitle(newInfo[0].text)
     else:
---
+++ utils/parseUtil.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+
+class ParseUtil:
+    def __init__(self, session, domain, soup=None):
+        self.session = session
+        self.domain  = domain
+        self.soup    = soup
+
+    def getSoup(self, method, path, data=None):
+        self.soup = BeautifulSoup(
+            self.session.request(method, self.domain + path, data=data).text, 'html.parser')
+        return self
+        
+    def wget(self, path, file):
+        data = self.session.request('GET', self.domain + path).content
+        try:
+            with open(file ,mode='wb') as f:
+                f.write(data)
+            return True
+        except:
+            return False
+    
+    def selectInSoup(self, selector):
+        return self.soup.select(selector) if self.soup != None else None
+    
+    def findInSoup(self, target, attrs=None):
+        return self.soup.find(target, attrs=attrs) if self.soup != None else None
